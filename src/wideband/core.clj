b2def4c9154fe0ed6d7d43f86ccc1d32a6f4afbd
Emil Bengtsson <emil@emil0r.com>
2022-02-02 12:33:39 +0100

    Add successfull GPIO test

--- src/wideband/core.clj
+++ src/wideband/core.clj
@@ -1,6 +1,39 @@
-(ns wideband.core)
+(ns wideband.core
+  (:require [helins.linux.gpio :as gpio]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+
+(with-open [device (gpio/device "/dev/gpiochip0")
+            led-handle (gpio/handle device
+                                    {5 {:gpio/state false
+                                        :gpio/tag :led1}
+                                     12 {:gpio/state false
+                                         :gpio/tag :led2}}
+                                    {:gpio/direction :output})
+            button-watcher (gpio/watcher device
+                                         {6 {:gpio/tag :led1-button
+                                             :gpio/direction :input}})]
+  (let [buffer (gpio/buffer led-handle)]
+    (loop [counter 10
+           leds (cycle [:led1 :led2])]
+      (gpio/write led-handle
+                  (gpio/set-line+ buffer {(first leds) true
+                                          (second leds) false}))
+      
+      (if (zero? counter)
+        ;; cleanup
+        (gpio/write led-handle
+                    (gpio/set-line+ buffer {(first leds) false
+                                            (second leds) false}))
+        (do
+          (println "==>>BEFORE EVENT")
+          (println "event" (gpio/event button-watcher))
+          (println "AFTER EVENT<<==")
+          (Thread/sleep 200)
+          (println "recycling")
+          (recur (dec counter) (rest leds)))))))
