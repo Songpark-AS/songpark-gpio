b43cf3f3ad025aa0658ff04c3ad0f69879ec92aa
Emil Bengtsson <emil@emil0r.com>
2022-05-15 14:26:14 +0200

    Rename project from songpark/bitbang to songpark/gpio

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject songpark/bitbang "1.0.0-SNAPSHOT"
-  :description "Bit Bang implementation for Songpark"
+(defproject songpark/gpio "1.0.0-SNAPSHOT"
+  :description "GPIO implementation for Songpark TPX"
   :url ""
   :license {:name ""
             :url ""}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [io.helins/linux.gpio "2.0.1"]]
-  :repl-options {:init-ns songpark.bitbang})
+  :repl-options {:init-ns songpark.gpio})
